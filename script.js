9f4895b6546a7b53bb303f1d7720ddfd087d4554
rejoy97 <wanniangrejoywell@gmail.com>
2025-01-31 19:52:23 +0530

    Add files via upload
    Added website files

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("joinButton").addEventListener("click", function() {
+    window.location.href = "https://t.me/https://t.me/+qRHhq1Gdm0QzYzQ1"; // Replace with your Telegram channel link
+});
