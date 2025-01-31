7f22db7c669cfe94b6dd58659976d7b703bf7bec
rejoy97 <wanniangrejoywell@gmail.com>
2025-01-31 22:09:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("joinButton").addEventListener("click", function() {
     window.location.href = "https://t.me/https://t.me/+qRHhq1Gdm0QzYzQ1"; // Replace with your Telegram channel link
 });
+alert("JavaScript is connected!");
